1b6ce33273444ec1e76a16edd5b94ecc1debc500
Morio <xtarako@gmail.com>
2023-05-04 19:16:59 +0900

    feat: fetch 1 week events from now

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,48 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { Env, Hono, HonoRequest } from "hono";
+import { getGoogleAuthToken } from "./lib/auth";
+import { calendar_v3 } from "@googleapis/calendar";
 
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-}
-
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("Hello World!");
-  },
+type Bindings = {
+  google_email: string;
+  google_private_key: string;
+  google_calendar_id: string;
 };
+
+const app = new Hono<{
+  Bindings: Bindings;
+}>();
+
+app.get("/", async (c) => {
+  const scope = "https://www.googleapis.com/auth/calendar";
+  const res = await getGoogleAuthToken(c.env.google_email, c.env.google_private_key, scope);
+
+  const query = {
+    timeMin: new Date().toISOString(),
+    timeMax: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString(),
+    singleEvents: "true",
+    orderBy: "startTime",
+  };
+
+  const qs = new URLSearchParams(query).toString();
+  const url = `https://www.googleapis.com/calendar/v3/calendars/${c.env.google_calendar_id}/events?${qs}`;
+
+  if (res.success) {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${res.data}`,
+      },
+    });
+    const events = (await response.json()) as calendar_v3.Schema$Events;
+    return c.text(JSON.stringify(events.items));
+  } else {
+    console.log("error", res.error);
+    c.text("Error");
+  }
+});
+
+export default app;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+// https://gist.github.com/markelliot/6627143be1fc8209c9662c504d0ff205
+
 function objectToBase64url(object: object) {
   return arrayBufferToBase64Url(new TextEncoder().encode(JSON.stringify(object)));
 }
@@ -20,10 +22,11 @@ function str2ab(str: string) {
 
 async function sign(content: string, signingKey: string) {
   const buf = str2ab(content);
+
   const plainKey = signingKey
     .replace("-----BEGIN PRIVATE KEY-----", "")
     .replace("-----END PRIVATE KEY-----", "")
-    .replace(/(\r\n|\n|\r)/gm, "");
+    .replace(/\\r\\n|\\n|\\r/gm, "");
   const binaryKey = str2ab(atob(plainKey));
   const signer = await crypto.subtle.importKey(
     "pkcs8",
@@ -39,7 +42,7 @@ async function sign(content: string, signingKey: string) {
   return arrayBufferToBase64Url(binarySignature);
 }
 
-const getGoogleAuthToken = async (user: string, key: string, scope: string): PromiseResult<string> => {
+export const getGoogleAuthToken = async (user: string, key: string, scope: string): PromiseResult<string> => {
   const jwtHeader = objectToBase64url({ alg: "RS256", typ: "JWT" });
   try {
     const assertionTime = Math.round(Date.now() / 1000);
@@ -53,7 +56,8 @@ const getGoogleAuthToken = async (user: string, key: string, scope: string): Pro
     });
 
     const jwtUnsigned = `${jwtHeader}.${claimSet}`;
-    const signedJwt = `${jwtUnsigned}.${sign(jwtUnsigned, key)}`;
+    const signature = await sign(jwtUnsigned, key);
+    const signedJwt = `${jwtUnsigned}.${signature}`;
     const body = `grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=${signedJwt}`;
     const response = await fetch("https://oauth2.googleapis.com/token", {
       method: "POST",
@@ -64,6 +68,7 @@ const getGoogleAuthToken = async (user: string, key: string, scope: string): Pro
       },
       body,
     });
+
     const { access_token } = (await response.json()) as { access_token: string };
     return {
       success: true,
